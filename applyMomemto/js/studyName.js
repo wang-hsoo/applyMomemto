ea7161848d591591d137ca48a9f36cb3d3c1a24a
wang-hsoo <64819868+wang-hsoo@users.noreply.github.com>
2022-06-24 08:04:57 +0900

    Add files via upload

--- applyMomemto/js/studyName.js
+++ applyMomemto/js/studyName.js
@@ -3,6 +3,7 @@
  const studyInput = new Array();
  let saveStudy = ["", "", "", "", "", ""];
 
+ //localStorage에 저장될 키 값
  const STUDYNAME = [
     "study1",
     "study2",
@@ -13,8 +14,11 @@
 ];
 
  for(let i = 0; i < STUDYNAME.length; i++){
+    //제목입략 form
     studyForm[i] = document.querySelector(`#study${i+1}`);
+    //입력한 제목을 보여주는 부분
     writeStudy[i] = document.querySelector(`#stName${i+1}`);
+    //제목을 입력하는 부분
     studyInput[i] = document.querySelector(`#study${i+1} input`);
 }
 
@@ -23,10 +27,14 @@ function getStudy(event){
     event.preventDefault();
     for(let i = 0; i < STUDYNAME.length; i++){
         if(studyInput[i].value !== ""){
+            //해당 input에 값이 있으면 Input을 hidden
             studyInput[i].classList.add("hidden");
+            //해당 값을 localstorage에 저장
             const studyName = studyInput[i].value;
             localStorage.setItem(STUDYNAME[i], studyName);
+            //해당 값을 데이터 확인을 위한 배열에 입력
             saveStudy[i] = localStorage.getItem(STUDYNAME[i]);
+         
             paintStudy(studyName);
         }
 
@@ -36,6 +44,7 @@ function getStudy(event){
 
 function paintStudy(study){
     for(let i = 0; i <= 5; i++){
+        //저장된 값이랑 받아온 값이 같으면 h2에 깂을 입력하고 hidden 을 지움
         if(saveStudy[i] === study){
             writeStudy[i].innerText = study;
             writeStudy[i].classList.remove("hidden");
@@ -48,15 +57,18 @@ function paintStudy(study){
 
 
 for(let i = 0; i < STUDYNAME.length; i++){
+    //해당 키 값에 localStorage에 데이터를 불러옴
     loadName = localStorage.getItem(STUDYNAME[i]);
     saveStudy[i] = localStorage.getItem(STUDYNAME[i]);
     
     
     if(loadName !== null){
+        //데이터가 있을 시 input 창을 hidden
         studyInput[i].classList.add("hidden");
         paintStudy(loadName);
     }
 
+    //제목이 있을 경우 제목을 클릭하면 저장된 데이터가 지워지고 다른 이름 설정 가능
     writeStudy[i].addEventListener("click", () => {
         localStorage.removeItem(STUDYNAME[i]);
         writeStudy[i].classList.add("hidden");
--- applyMomemto/js/todo.js
+++ applyMomemto/js/todo.js
@@ -16,9 +16,13 @@ const TODONAME = [
 ];
 
 for(let i = 0; i < TODONAME.length; i++){
+    //리스트 입력 form
     toDoForm[i] = document.querySelector(`#todo-form${i+1}`);
+    //리스트 입력 부분
     toDoInput[i] = document.querySelector(`#todo-form${i+1} input`);
+    //리스트 목록 ul
     toDoList[i] = document.querySelector(`#todo-list${i+1}`);
+    //목록을 저장할 배열
     toDos[i] = new Array();
  }
 
@@ -32,9 +36,11 @@ for(let i = 0; i < TODONAME.length; i++){
 
 
  function deleteToDo(event){
+    //클릭된 element 저장
     const li = event.target.parentElement;
 
     for(let i = 0; i < TODONAME.length; i++){
+        // 클릭된 li에 제외 한 새로운 배열 생성 후 해당 li 삭제
         toDos[i] = toDos[i].filter((toDo) => toDo.id !== parseInt(li.id));
         li.remove();
         saveToDos();
@@ -43,6 +49,7 @@ for(let i = 0; i < TODONAME.length; i++){
 
  function paintTodos(newTodo, index){
 
+        //화면에 출력할 리스트 생성
         const li = document.createElement("li");
         li.id = newTodo.id;
         const span = document.createElement("span");
@@ -65,8 +72,10 @@ for(let i = 0; i < TODONAME.length; i++){
     event.preventDefault();
      for(let i = 0; i < TODONAME.length; i++){
         if(toDoInput[i].getText !== ""){
+            //input에 값이 있을시 해당 값을 변수에 저장하고 input에 다시 입력할 수 있도록 값을 초기화 해준다
             const newTodo = toDoInput[i].value;
             toDoInput[i].value = "";
+            //삭제시 정확한 목록을 알기위해 데이터와 현재 Date를 id로 준다
             const newTodoObj = {
                 text: newTodo,
                 id: Date.now()
@@ -75,6 +84,7 @@ for(let i = 0; i < TODONAME.length; i++){
             
             if(newTodoObj.text !== ""){
                 toDos[i].push(newTodoObj);
+                //입력되야 할 form을 알아야하기 때문에 index도 같이 전달
                 paintTodos(newTodoObj, i);
                 saveToDos();
             }
@@ -87,16 +97,20 @@ for(let i = 0; i < TODONAME.length; i++){
      toDoForm[i].addEventListener("submit", handleToDosubmit, false);   
 
      const savedToDos = new Array();
+     //localstorage에 있는 값들을 변수에 저장
+     //saveToDos는 2차원 배열
      savedToDos[i] = JSON.parse(localStorage.getItem(TODONAME[i]));
 
      for(let e = 0; e < savedToDos[i].length; e++){
-
+        //데이터가 있을시 객체로 저장
         if(savedToDos[i][e].text !== null){
             const newTodoObj = {
                 text: savedToDos[i][e].text,
                 id: savedToDos[i][e].id
             };
+            
             toDos[i].push(newTodoObj);
+            //입력되야 할 form을 알아야하기 때문에 index도 같이 전달
             paintTodos(newTodoObj, i);
 
         }
